7f9c3c6a051cded596771d443cb0e2924a8766fb
Michael Pearson <mfpears@gmail.com>
2022-10-24 21:40:22 -0600

    refactor(app-shell): Cleanup

--- projects/movies/src/app/app-shell/app-shell.component.ts
+++ projects/movies/src/app/app-shell/app-shell.component.ts
@@ -1,4 +1,3 @@
-import { RxState } from '@rx-angular/state';
 import { CommonModule, DOCUMENT } from '@angular/common';
 import {
   ChangeDetectionStrategy,
@@ -19,11 +18,9 @@ import {
 import { TMDBMovieGenreModel } from '../data-access/api/model/movie-genre.model';
 import { fallbackRouteToDefault } from '../shared/router/routing-default.util';
 import { trackByProp } from '../shared/utils/track-by';
-import { RxActionFactory } from '@rx-angular/state/actions';
 import { RouterState } from '../shared/router/router.state';
 import { getIdentifierOfTypeAndLayoutUtil } from '../shared/router/get-identifier-of-type-and-layout.util';
 import { GenreResource } from '../data-access/api/resources/genre.resource';
-import { RxEffects } from '@rx-angular/state/effects';
 import { HamburgerButtonComponent } from '../ui/component/hamburger-button/hamburger-button.component';
 import { LetModule } from '@rx-angular/template/let';
 import { SideDrawerComponent } from '../ui/component/side-drawer/side-drawer.component';
